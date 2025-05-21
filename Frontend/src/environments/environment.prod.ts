1463933aadcb84c486783d7dbcc1e23508e2efd3
Isaak Genge <isaak.genge@fusiondatahub.com>
2025-05-21 12:38:37 -0600

    fix up build a bit

--- Frontend/src/environments/environment.prod.ts
+++ Frontend/src/environments/environment.prod.ts
@@ -3,6 +3,6 @@ import { Environment } from './environment.interface';
 
 export const environment: Environment = {
   production: true,
-  apiBaseUrl: 'http://localhost:5447/api' // Update with production URL
+  apiBaseUrl: 'https://modelerapiapiservice20250521101918-cjggfabua3f8cxap.eastus-01.azurewebsites.net/api' // Update with production URL
 };
 
